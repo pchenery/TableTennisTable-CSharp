5ebd2519baf7896576b7e55f9da647025c51e28c
Peter Chenery <peter.chenery@anthonynolan.org>
2018-10-18 19:32:41 +0100

    First Tests

--- TableTennisTable-Tests/LeagueTests.cs
+++ TableTennisTable-Tests/LeagueTests.cs
@@ -23,5 +23,51 @@ namespace TableTennisTable_Tests
             Assert.AreEqual(1, firstRowPlayers.Count);
             CollectionAssert.Contains(firstRowPlayers, "Bob");
         }
+
+        [TestMethod]
+        public void TestGetRows()
+        {
+            // Given
+            League league = new League();
+
+            // When
+            league.AddPlayer("Bob");
+            league.AddPlayer("Ted");
+
+            // Then
+            var rows = league.GetRows();
+            Assert.AreEqual(2, rows.Count);
+        }
+
+        [TestMethod]
+        public void TestRecordWin()
+        {
+            // Given
+            League league = new League();
+
+            // When
+            league.AddPlayer("Bob");
+            league.AddPlayer("Ted");
+
+            // Then
+
+        }
+
+        [TestMethod]
+        public void TestGetWinner()
+        {
+            // Given
+            League league = new League();
+
+            // When
+            league.AddPlayer("Bob");
+            league.AddPlayer("Ted");
+
+            var rows = league.GetRows();
+
+            // Then
+            Assert.AreEqual("Bob", rows.First().GetPlayers().First());
+            Assert.AreNotEqual("Ted", rows.First().GetPlayers().First());
+        }
     }
 }
